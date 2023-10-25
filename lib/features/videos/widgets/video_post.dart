60ea17cdb8d4e272708738e1f1e7cfb1240677be
topcircle <topcircle@gmail.com>
2023-10-25 12:22:20 +0900

    #7.5 AnimatedController

--- lib/features/videos/widgets/video_post.dart
+++ lib/features/videos/widgets/video_post.dart
@@ -18,9 +18,16 @@ class VideoPost extends StatefulWidget {
   State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VideoPostState extends State<VideoPost> {
+class _VideoPostState extends State<VideoPost>
+    with SingleTickerProviderStateMixin {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset("assets/videos/video.mp4");
+
+  final Duration _animationDuration = const Duration(milliseconds: 200);
+
+  late final AnimationController _animationController;
+
+  bool _isPaused = false;
 
   void _onVideoChange() {
     if (_videoPlayerController.value.isInitialized) {
@@ -33,15 +40,25 @@ class _VideoPostState extends State<VideoPost> {
 
   void _initVideoPlayer() async {
     await _videoPlayerController.initialize();
-    // _videoPlayerController.play();
-    setState(() {});
     _videoPlayerController.addListener(_onVideoChange);
+    setState(() {});
   }
 
   @override
   void initState() {
     super.initState();
     _initVideoPlayer();
+
+    _animationController = AnimationController(
+      vsync: this,
+      lowerBound: 1.0,
+      upperBound: 1.5,
+      value: 1.5,
+      duration: _animationDuration,
+    );
+    _animationController.addListener(() {
+      setState(() {});
+    });
   }
 
   void _onVisibilityChanged(VisibilityInfo info) {
@@ -51,14 +68,23 @@ class _VideoPostState extends State<VideoPost> {
   }
 
   void _onTogglePause() {
-    _videoPlayerController.value.isPlaying
-        ? _videoPlayerController.pause()
-        : _videoPlayerController.play();
+    if (_videoPlayerController.value.isPlaying) {
+      _videoPlayerController.pause();
+      _animationController.reverse();
+    } else {
+      _videoPlayerController.play();
+      _animationController.forward();
+    }
+
+    setState(() {
+      _isPaused = !_isPaused;
+    });
   }
 
   @override
   void dispose() {
     _videoPlayerController.dispose();
+    _animationController.dispose();
     super.dispose();
   }
 
@@ -81,13 +107,20 @@ class _VideoPostState extends State<VideoPost> {
               onTap: _onTogglePause,
             ),
           ),
-          const Positioned.fill(
+          Positioned.fill(
               child: IgnorePointer(
             child: Center(
-              child: FaIcon(
-                FontAwesomeIcons.play,
-                color: Colors.white,
-                size: Sizes.size52,
+              child: Transform.scale(
+                scale: _animationController.value,
+                child: AnimatedOpacity(
+                  opacity: _isPaused ? 1 : 0,
+                  duration: _animationDuration,
+                  child: const FaIcon(
+                    FontAwesomeIcons.play,
+                    color: Colors.white,
+                    size: Sizes.size52,
+                  ),
+                ),
               ),
             ),
           ))
