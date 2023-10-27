115c1ea72720392a0243aa1980ceb3a7c8eb3ba1
topcircle <topcircle@gmail.com>
2023-10-27 18:10:11 +0900

    #19.2 CameraCOntroller

--- lib/features/authentication/sign_up_screen.dart
+++ lib/features/authentication/sign_up_screen.dart
@@ -3,7 +3,6 @@ import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:flutter_tiktok_clone/constants/gaps.dart';
 import 'package:flutter_tiktok_clone/constants/sizes.dart';
 import 'package:flutter_tiktok_clone/features/authentication/username_screen.dart';
-import 'package:flutter_tiktok_clone/features/authentication/login_screen.dart';
 import 'package:flutter_tiktok_clone/features/authentication/widgets/auth_button.dart';
 import 'package:flutter_tiktok_clone/generated/l10n.dart';
 import 'package:flutter_tiktok_clone/utils.dart';
