90c47b4272eb98a7b8a09bc5cfc62f515e3194eb
topcircle <topcircle@gmail.com>
2023-11-04 18:09:12 +0900

    Update integration_test.dart

--- test/integration_test.dart
+++ test/integration_test.dart
@@ -1,4 +1,4 @@
-/* import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -66,4 +66,3 @@ void main() {
     await tester.pumpAndSettle(const Duration(seconds: 10));
   });
 }
- */
